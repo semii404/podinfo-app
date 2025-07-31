b6c38a525de63438f5139d89665e0005f3c34537
saleem404 <cs17saleem.m1997@gmail.com>
2025-07-31 15:03:50 +0530

    update script

--- apps/logging/logging-script.sh
+++ apps/logging/logging-script.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
+# Capture any error
+trap 'echo "Error occurred!"; exit 1' ERR
+
 # Infinite loop
 while true
 do
-    # Print a message to the console
     echo "Hello! I will wait for 5 seconds..."
-
-    # Wait for 5 seconds
     sleep 5
-
-    # Print a message after waiting
     echo "5 seconds have passed!"
-
-    # Optional: Add a slight delay between iterations if needed
-    # sleep 1
 done
