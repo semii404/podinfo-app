a7b1613bbaec28e647cc670f7fe8702bed46ff69
saleem404 <cs17saleem.m1997@gmail.com>
2025-07-31 13:51:59 +0530

    test script mapping

---
+++ apps/logging/logging-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Print a message to the console
+echo "Hello! I will wait for 5 seconds..."
+
+# Wait for 5 seconds
+sleep 5
+
+# Print a message after waiting
+echo "5 seconds have passed!"
